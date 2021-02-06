aa703b5d90bb72659f40f418942e2666599dc7c5
Njord0 <52111877+Njord0@users.noreply.github.com>
2021-02-06 17:50:45 +0100

    Improve prn parameters and delete globals variables

--- capture_handshake.py
+++ capture_handshake.py
@@ -13,6 +13,9 @@ import logging
 import os
 import subprocess
 import sys
+import typing
+
+from collections import OrderedDict 
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
@@ -24,43 +27,42 @@ parser.add_argument("-o", "--output", help="Output file", required=False)
 
 args = parser.parse_args()
 
-def eapol_filter(packet):
-    global packet_1, packet_2, packet_3, packet_4, packet_5
+def eapol_filter(packets):
+    def eapol(packet):
+        if packet.haslayer(EAPOL):
+            if args.bssid.lower() in [packet.addr1, packet.addr2, packet.addr3]:
+                packet_raw = binascii.hexlify(packet[Raw].load)
+            else:
+                return
 
-    if packet.haslayer(EAPOL):
-        if args.bssid.lower() in [packet.addr1, packet.addr2, packet.addr3]:
-            packet_raw = binascii.hexlify(packet[Raw].load)
-        else:
-            return
+            if packet_raw[2:6].decode() == "008a": #message_1
+                packets["packet_1"] = packet
 
-        if packet_raw[2:6].decode() == "008a": #message_1
-            packet_1 = packet
+            elif packet_raw[2:6].decode() == "010a": #message_2
+                packets["packet_2"] = packet
 
-        elif packet_raw[2:6].decode() == "010a": #message_2
-            packet_2 = packet
+            elif packet_raw[2:6].decode() == "13ca": #message_3 
+                packets["packet_3"] = packet
 
-        elif packet_raw[2:6].decode() == "13ca": #message_3 
-            packet_3 = packet
+            elif packet_raw[2:6].decode() == "030a": #message_4
+                packets["packet_4"] = packet
 
-        elif packet_raw[2:6].decode() == "030a": #message_4
-            packet_4 = packet
+        elif packet.haslayer(Dot11Beacon):
+            if args.bssid.lower() in [packet.addr1, packet.addr2, packet.addr3]:
+                packets["packet_5"] = packet
 
-    elif packet.haslayer(Dot11Beacon):
-        if args.bssid.lower() in [packet.addr1, packet.addr2, packet.addr3]:
-            packet_5 = packet
+        if all(list(packets.values())):
+            print("\n[+] 4-Way handshake captured, now saving...")
 
-    if packet_1 and packet_2 and packet_3 and packet_4 and packet_5:
-        print("\n[+] 4-Way handshake captured, now saving...")
+            try:
+                wrpcap("wpa_handshake01.pcap" if not args.output else args.output, 
+                    list(packets.values()))
+                sys.exit("[+] Done.\n")
 
-        PACKET_LIST = [packet_1, packet_2, packet_3, packet_4, packet_5]
-        try:
-            wrpcap("wpa_handshake01.pcap" if not args.output else args.output, PACKET_LIST)
-            sys.exit("[+] Done.\n")
+            except Exception as e:
+                print(e)
 
-        except Exception as e:
-            print(e)
-
-packet_1 = packet_2 = packet_3 = packet_4 = packet_5 = False
+    return eapol
 
 def main(interface, bssid, channel, output):
 
@@ -70,7 +72,11 @@ def main(interface, bssid, channel, output):
     print("[+] Writing file {}".format("wpa_handshake01.pcap" if not output else output))
     print("[+] Capturing 4-Way handshake [{}]...".format(bssid))
 
-    sniff(prn=eapol_filter)
+    packets = OrderedDict(zip(
+        [f"packet_{i}" for i in range(1, 6)], 
+        [False]*5))
+
+    sniff(prn=eapol_filter(packets))
 
 if __name__ == "__main__":
 
